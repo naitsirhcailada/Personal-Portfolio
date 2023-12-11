cbf9ef0f25e095dc35a6adb986c09b9cee1c0aa3
Christian Adalia <jmcerezo@integranet.ph>
2023-12-11 11:16:47 +0800

    portfolio

--- javascript/switchmodecontact.js
+++ javascript/switchmodecontact.js
@@ -5,7 +5,7 @@ function darkmode() {
   document.getElementById('active').style.display = "block";
   document.getElementById('light').style.display = "block";
   document.getElementById('container').style.background = "#363062";
-
+  document.getElementById('planeicon').style.color = "#f5f5f5";
   const root = document.querySelector(':root');
   root.style.setProperty('--black', '#f5f5f5');
   root.style.setProperty('--tertiarycolor', '#435585');
@@ -17,7 +17,7 @@ function lightmode() {
   document.getElementById('light').style.display = "block";
   document.getElementById('active').style.display = "block";
   document.getElementById('container').style.background = "#D4D6B9";
-
+  document.getElementById('planeicon').style.color = "#383B53";
 
   const root = document.querySelector(':root');
   root.style.setProperty('--black', '#000');
